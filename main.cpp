59b5131431a53b5c711484739163843637f6abfd
Korolenkova <varya35@yandex.ru>
2015-10-24 10:28:09 +0400

    New file

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "sdt.h"
+int main ()
+{
+    cout<<"Enter number 1: ";
+    int n;
+    cin>>n;
+    cout<<"Enter number 2:";
+    int m;
+    cin>>m;
+    int s;
+    s=n+m;
+    cout<<"Summ: "<<s<<endl;
+}
